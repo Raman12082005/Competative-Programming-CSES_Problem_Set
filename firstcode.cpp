5368b21c7f5c1c52175455d7d666c285c5721b82
Raman12082005 <rdeep1208y@gmail.com>
2026-01-03 15:55:18 +0530

    Add files via upload

---
+++ firstcode.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    set<int> s;
+    int temp;
+    for(int i=0; i<n; i++){
+        
+        cin>>temp;
+
+        s.insert(temp);
+    }
+    cout<<s.size();
+}
